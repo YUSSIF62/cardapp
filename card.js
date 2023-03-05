baea9ce7d98ff7b5b390d1907a0797c82805425d
Abdul-Rahaman Yussif <abdulrahamanyussif62@gmail.com>
2023-03-05 13:54:06 +0000

    a

--- card.js
+++ card.js
@@ -1,11 +1,28 @@
 let firstCard= 4
 let secondCard = 6
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message
 
-if ( sum =>15){
-    console.log("Do you want to draw a card? ")
-} else if (sum== 15){
-    console.log("wow, you had a black card! ")
-} else if (sum <= 15){
-    console.log("You are no  in the game again!! ")
+let messagEl = document.getElementById("message-el")
+console.log(messagEl)
+
+let sumEl= document.getElementById("sum-el")
+let cardEl= document.getElementById("card-el")
+
+function startGame() {
+    cardEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if ( sum =>15){
+        message= "Do you want to draw a card? "
+    } else if (sum== 15){
+        message= "wow, you had a black card! " 
+        hasBlackJack = true
+    } else {
+        message = "You are no  in the game again!! " 
+        isAlive = false
+    }
+    
+    messagEl.textContent= message
 }
