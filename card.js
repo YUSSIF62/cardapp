b10fe95f6106d8adb7008f2eab6e3fcfca650d5f
Abdul-Rahaman Yussif <abdulrahamanyussif62@gmail.com>
2023-03-05 07:40:43 +0000

    Update card.js

--- card.js
+++ card.js
@@ -3,5 +3,9 @@ let secondCard = 6
 let sum = firstCard + secondCard
 
 if ( sum =>15){
-    console.log()
+    console.log("Do you want to draw a card? ")
+} else if (sum== 15){
+    console.log("wow, you had a black card! ")
+} else if (sum <= 15){
+    console.log("You are no  in the game again!! ")
 }
